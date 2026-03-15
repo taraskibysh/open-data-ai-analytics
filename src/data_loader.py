4c07c1d827166491499fd2f7b11ffd30fdd54367
Taras <146379503+taraskibysh@users.noreply.github.com>
2026-03-15 20:21:31 +0200

    Сhore: update ci (#13)
    * chore: update ci
    
    * chore: fix bug with matrix setup
    
    * chore: add requirements file for ci
    
    * chore: add mising imports
    
    * chore: configure sequential module execution
    
    * feat: remove try-except
    
    * feat: fix data save dir
    
    * feat: update data link
    
    * feat: update data_loader script
    
    * chore: update requirement imports

--- src/data_loader.py
+++ src/data_loader.py
@@ -1,8 +1,10 @@
 import requests
 from pathlib import Path
 
-DATA_URL = "https://data.gov.ua/dataset/ffc99c1e-ac64-4b8f-ad97-858060cdb012/resource/dbfcbad0-3fc9-41b2-a5ad-5c7f25dbacda"
-DATA_DIR = Path("data/raw")
+DATA_URL = "https://data.gov.ua/dataset/1077e470-bfcb-434e-ab41-1a95c1f527ce/resource/dbfcbad0-3fc9-41b2-a5ad-5c7f25dbacda/download/publichna-informatsiia-vikhidni-za-gruden-2025.xlsx"
+
+SCRIPT_DIR = Path(__file__).resolve().parent
+DATA_DIR = SCRIPT_DIR.parent / "data" / "raw"
 DATA_DIR.mkdir(parents=True, exist_ok=True)
 FILE_PATH = DATA_DIR / "publichna-informatsiia-vikhidni-za-gruden-2025.xlsx"
 
@@ -10,8 +12,10 @@ def download_dataset(url: str, path: Path):
     print(f"Downloading dataset from {url} ...")
     response = requests.get(url)
     response.raise_for_status()
+
     with open(path, "wb") as f:
         f.write(response.content)
+
     print(f"Dataset saved to {path}")
 
 if __name__ == "__main__":
