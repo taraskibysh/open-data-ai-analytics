256db195ecd3f6aa166fede664e76485d76c2422
Kibysh Taras <taras.kibysh,shi.2023@lpnu.ua>
2026-03-15 19:41:45 +0200

    feat: fix data save dir

--- src/data_loader.py
+++ src/data_loader.py
@@ -2,7 +2,7 @@ import requests
 from pathlib import Path
 
 DATA_URL = "https://data.gov.ua/dataset/ffc99c1e-ac64-4b8f-ad97-858060cdb012/resource/dbfcbad0-3fc9-41b2-a5ad-5c7f25dbacda"
-DATA_DIR = Path("data/raw")
+DATA_DIR = Path("../data/raw")
 DATA_DIR.mkdir(parents=True, exist_ok=True)
 FILE_PATH = DATA_DIR / "publichna-informatsiia-vikhidni-za-gruden-2025.xlsx"
 
